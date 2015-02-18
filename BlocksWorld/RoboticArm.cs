50798f7ca3e23193f42acd1c58d4e178404df4b1
Christian Lucero <crizlucero@gmail.com>
2015-02-18 17:25:07 -0600

    Agregando las reglas al brazo robótico

--- BlocksWorld/RoboticArm.cs
+++ BlocksWorld/RoboticArm.cs
@@ -3,10 +3,42 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
 
 namespace BlocksWorld
 {
-    class RoboticArm
+    class RoboticArm : Rules
     {
+        private Grid initEnv { get; set; }
+        private Grid curEnv { get; set; }
+
+        public RoboticArm(Grid Environment)
+        {
+            this.initEnv = Environment;
+            this.curEnv = Environment;
+        }
+
+        public bool Stack(Label X, Label Y)
+        {
+            //Precondition
+            if (Clear(Y) && Holding(X))
+            {
+
+                Grid.SetRow(X, Grid.GetRow(Y) - 1);
+                Grid.SetColumn(X, Grid.GetColumn(Y));
+                //Add
+                if (ArmEmpty(X) && On(X, Y))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Unstack(Label X, Label Y);
+
+        public void PickUp(Label X);
+
+        public void PutDown(Label X);
     }
 }
--- BlocksWorld/Rules.cs
+++ BlocksWorld/Rules.cs
@@ -10,29 +10,54 @@ namespace BlocksWorld
 {
     class Rules
     {
+        /// <summary>
+        /// El bloque 1 está encima del bloque 2
+        /// </summary>
+        /// <param name="X">Bloque 1</param>
+        /// <param name="Y">Bloque 2</param>
+        /// <returns>Si el bloque 1 está encima del bloque 2</returns>
         public bool On(Label X, Label Y)
         {
             if (X.IsVisible && (Grid.GetRow(X) == (Grid.GetRow(Y) - 1)))
                 return true;
             return false;
         }
+        /// <summary>
+        /// El bloque está en la mesa
+        /// </summary>
+        /// <param name="X">Bloque</param>
+        /// <returns>Si el bloque está en la mesa</returns>
         public bool OnTable(Label X)
         {
             if (X.IsVisible)
                 return true;
             return false;
         }
+        /// <summary>
+        /// Nada arriba de la caja
+        /// </summary>
+        /// <param name="X">Caja</param>
+        /// <returns>Si existe algo o no</returns>
         public bool Clear(Label X)
         {
             return false;
         }
+        /// <summary>
+        /// El brazo robótico sostiene al bloque
+        /// </summary>
+        /// <param name="X">Bloque</param>
+        /// <returns>Si el brazo sostiene al bloque</returns>
         public bool Holding(Label X)
         {
             if (X.IsVisible && (Grid.GetRow(X) == 1 && Grid.GetColumn(X) == 3))
                 return true;
             return false;
         }
-
+        /// <summary>
+        /// El brazo está desocupado
+        /// </summary>
+        /// <param name="X">Bloque</param>
+        /// <returns>Si el brazo está desocupado</returns>
         public bool ArmEmpty(Label X)
         {
             if (X.IsVisible && (Grid.GetRow(X) == 1 && Grid.GetColumn(X) == 3))
